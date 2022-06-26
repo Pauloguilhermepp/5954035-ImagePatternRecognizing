3290541027a668b895fe37a694a1a7731c0fb945
Paulo Guilherme <pauloguilherme@usp.br>
2022-06-26 01:12:03 -0300

    Update Image class

--- src/Plugin/Image.java
+++ src/Plugin/Image.java
@@ -1,8 +1,21 @@
+import java.util.*;
+import java.io.*;
 
-public class Image{
+public class Image implements Comparable<Image>{
+    public double dist;
     public MetricHist mh;
     public String imageName, mhStr;
+
     public static void main(String []args){
         System.out.println("oi");
     }
+
+    public double getDist(){
+        return dist;
+    }
+
+    public int compareTo(Image img) {
+        return Integer.compare((int)(getDist()), (int)(img.getDist()));
+    }
+
 }
